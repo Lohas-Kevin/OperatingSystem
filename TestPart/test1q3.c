e9c697e743573c7557e0515224012a4b5772d76d
Lohas-Kevin <cheny43@rpi.edu>
2019-02-25 10:33:53 -0500

    Some practice test code

--- TestPart/test1q3.c
+++ TestPart/test1q3.c
@@ -3,19 +3,28 @@
 #include <string.h>
 #include <unistd.h>
 
-int main(){
+
+int main()
+{
+	char * a = "POLYTECHNIC";
+	char * b = a;
+	char * c = calloc( 100, sizeof( char ) );
+	printf( "[%s][%s][%s]\n", a + 10, b + 9, c + 8 );
+	char ** d = calloc( 100, sizeof( char * ) );
+	d[7] = calloc( 20, sizeof( char ) );
+	d[6] = c;
+	strcpy( d[7], b + 5 );
+	strcpy( d[6], b + 4 );
+	printf( "[%s][%s][%s]\n", d[7], d[6], c + 5 );
+	float e = 2.71828;
+	float * f = calloc( 1, sizeof( float ) );
+	float * g = f;
+	float * h = &e;
+	printf( "[%3.2f][%2.3f][%2.1f]\n", e, e, *h );
 	
-	char* a = calloc(7, sizeof(char));
-	*a = 'b';
-	*(a+1) = 'l';
-	*(a+2) = 'a';
-	*(a+3) = 'b';
-	*(a+4) = 'l';
-	*(a+5) = 'a';
-	*(a+6) = '\0';
-	char* b = a;
-	*(b+2) = 'x';
-	printf("[%s][%s]\n", a, b);
-	free(a);
-	
+	free(f);
+	free(d[7]);
+	free(c);
+	free(d);
+	return EXIT_SUCCESS;
 }
