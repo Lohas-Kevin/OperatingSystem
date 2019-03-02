8379e458d4c1e5a7c4ad2997e78f7eb159f75a0a
Lohas-Kevin <cheny43@rpi.edu>
2019-03-02 00:49:11 -0500

    Wait for solving IPC

--- HW2/HW2.c
+++ HW2/HW2.c
@@ -8,78 +8,363 @@
 #include <string.h>
 
 
+int checkPath(char* mypath, char* target, char* dic){
+
+	#ifdef DEBUG_MODE2
+	fprintf(stderr,"[%s],[%s],[%s]\n", mypath, target, dic);
+	#endif
+	
+	//check whether the path and target are valid
+	if(mypath == NULL || target == NULL || dic == NULL){
+		
+		#ifdef DEBUG_MODE
+		fprintf(stderr,"Error: check Path read NULL char* \n");
+		fprintf(stderr,"[%s],[%s],[%s]\n", mypath, target, dic);
+		#endif
+		
+		return -1;
+	}
+	//then check the path from left to right
+	else{
+		char* token = strtok(mypath, "#");
+		while(token != NULL){
+			char* temp = calloc(1024, sizeof(char));
+			strcpy(temp, token);
+			strcat(temp, "/");
+			strcat(temp, target);
+			
+			#ifdef DEBUG_MODE2
+			printf("the temp is: %s \n", temp);
+			#endif
+			
+			//check the path
+			struct stat sb;
+			int result = lstat(temp, &sb);
+			
+			//if find the path
+			if(result == 0 && (sb.st_mode & S_IXUSR)){
+				dic = strcpy(dic,temp);
+				free(temp);
+				
+				#ifdef DEBUG_MODE2
+				printf("find at: %s\n",dic);
+				#endif
+				
+				return 1;
+			}
+			//printf("mypath now is:[%s]\n",mypath);
+			
+			//renew the token and free memory
+			token = strtok(NULL,"#");
+			free(temp);
+		}
+		return 0;
+	}
+}
+
+int divideInput(char* input, char** argv){
+	
+	if( input == NULL || argv == NULL){
+		fprintf(stderr,"Error, DivideInput read NULL data\n");
+		fprintf(stderr,"[%s]\n", input);
+		return -1;
+	}
+	
+	//then seperate the original input string
+	int count = 0;
+	char* token = strtok(input, " ");
+	
+	while( token != NULL){
+		char* temp = malloc(strlen(token) + 1);
+		strcpy(temp, token);
+		argv[count] = temp;
+		count++;
+		token = strtok(NULL, " ");
+	}
+	return count;
+	
+}
+
+
+
 int main(){
 	
+	setvbuf( stdout, NULL, _IONBF, 0 );
+	
+	int has_pid1 = 0;
+	int has_pid2 = 0;
+	int bg_pid1 = -1;
+	int bg_pid2 = -1;
+	int status;
 	
 	while(1){
 		
-		//create the buffer to read the input
+		//check the background process
+		if(has_pid1 == 1){
+			pid_t bg1 = waitpid(bg_pid1, &status, WNOHANG);
+			if(bg1 > 0){
+				printf("[process %d terminated with exit status %d]\n", bg1, status);
+				has_pid1 = 0;
+				bg_pid1 = -1;
+			}
+		}
+		
+		if(has_pid2 == 1){
+			pid_t bg2 = waitpid(bg_pid2, &status, WNOHANG);
+			if(bg2 > 0){
+				printf("[process %d terminated with exit status %d]\n", bg2, status);
+				has_pid2 = 0;
+				bg_pid2 = -1;
+			}
+		}
+		
+		int background = 0;
+		int multiTask = 0;
+		
+		//get the current dir and print command
 		char* dir = (char*)calloc(1024, sizeof(char));
 		getcwd(dir, 1024);
-		printf("%s$ Enter the command:\n",dir);
-		char* rBuffer = (char*)calloc(1024, sizeof(char));
-		fgets(rBuffer,1024, stdin);
+		printf("%s$ ",dir);
+		free(dir);
+		
+		//read the user inputs
+		char* rBuffer = calloc(1024, sizeof(char));
+		rBuffer = fgets(rBuffer,1024,stdin);
+		rBuffer[strlen(rBuffer)-1] = '\0';
 		
 		
-		#ifdef DEBUG1
-		printf("You entered: %s", rBuffer);
-		printf("length of rBuffer is %ld\n", strlen(rBuffer));
+		//create the variables for input divide
+		char** argv = calloc(16, sizeof(char*));
+		int count;
+		//second set of variables if there is two processes
+		char** argv2 = calloc(16, sizeof(char*));
+		int count2;
+		
+		count = divideInput(rBuffer, argv);
+		int seppos = -1;
+		
+		//check is there any background or multi tasks
+		if(count >0){
+			
+			for(int i = 0; i < count; i++){
+				//check multitasks
+				//printf("[%s]\n", argv[i]);
+				if(strcmp(argv[i],"|") == 0){
+					multiTask = 1;
+					seppos = i;
+					
+				}
+			}
+			if( strcmp( argv[count-1], "&" ) == 0 ){
+				
+				background = 1;
+				free(argv[count-1]);
+				argv[count-1] = NULL;
+				count--;
+			}
+		}
+		
+		if(multiTask == 1 && seppos != -1){
+			
+			for(int i = seppos+1; i < count; i++){
+				char* arg2 = calloc(64, sizeof(char*));
+				arg2 = strcpy(arg2, argv[i]);
+				free(argv[i]);
+				argv2[i-seppos-1] = arg2;
+				count2++;
+			}
+			free(argv[seppos]);
+			count -= (count2+1);
+		}
+		
+		
+		#ifdef DEBUG_MODE
+		for(int i = 0; i < count; i++){
+			printf("the arg at [%d] is [%s]\n", i, argv[i]);
+		}
 		#endif
 		
-		
-		//if we read Exit
-		if(strcmp("exit\n", rBuffer) == 0){
-			printf("goodbye\n");
-			
-			//free the memory
-			free(rBuffer);
-			break;
-			
-		}else{
-			/*
-				over here we check the input
-				firstly, check the mypath variable
-			*/
-			char* mypath = calloc(1024, sizeof(char));
-			mypath = getenv("$MYPATH");
-			
-			//if $MYPATH is not set
-			if(strlen(mypath))
-			
-			
-		}	
-		
-		struct stat sb;
-		
-		strcat(dir, "/helloWorld10.exe");
-		lstat(dir, &sb);
-
-		
-		pid_t pid;
-		pid = fork();
-		
-		
-		if(pid == -1){
-			perror("fork failed\n");
-			return EXIT_FAILURE;
-		}
-		
-		if(pid == 0){
-			
-			char* argv[] = {"helloWorld10.exe",NULL};
-			execv(dir, argv);
-			
-		}
-		else{
-			int status;
-			pid_t child_pid = waitpid(pid,&status,0);
-			printf("[process %d terminated with exit status %d]\n", child_pid, status);
-			
-			
-		}
-		
-		free(dir);
 		free(rBuffer);
+		
+		//create the memory block for lstat and execv
+		
+		int result = -1;
+		int result2 = -1;
+		char* path = calloc(1024, sizeof(char));
+		char* path2 = calloc(1024, sizeof(char));
+		char* mypath = calloc(1024, sizeof(char));
+		
+		
+		if(count > 0){
+			//if we read Exit
+			if(strcmp("exit", argv[0]) == 0){
+				printf("bye\n");
+		
+				//free the memory
+				for(int i = 0; i < count; i++){
+					free(argv[i]);
+				}
+				free(argv);
+				free(path);
+				free(mypath);
+				break;
+				
+			}
+			//if we read cd
+			else if(strcmp("cd", argv[0]) == 0){
+				 
+				if(count == 1){
+					//if there is no following argument
+					chdir(getenv("HOME"));
+				}else{
+					chdir(argv[1]);
+				}
+			}
+			//if we doesn't read Exit
+			else{
+				
+				//over here we check the input
+				if(getenv("MYPATH") == NULL){
+					//if $MYPATH is NULL
+					//we set $MYPATH to default path
+					//Then use the function to check the lstat
+					
+					mypath = strcpy(mypath, "/bin#.");
+					result = checkPath(mypath, argv[0], path);
+					
+					if(multiTask == 1){
+						result2 = checkPath(mypath, argv2[0], path2);
+					}
+					
+				}
+				//if $MYPATH is set
+				else{
+					
+					mypath = getenv("MYPATH");
+					result = checkPath(mypath, argv[0], path);
+					
+					if(multiTask == 1){
+						result2 = checkPath(mypath, argv2[0], path2);
+					}
+				}
+				
+			}
+		}
+		//after checking the lstat result, start fork
+		//if there is a executable found
+		
+		if(result == 1 ){
+			
+			int p[2];
+			int rc = pipe(p);
+			
+			if(rc == -1){
+				perror("Error: Pipe Failed\n");
+			}
+			
+			
+			pid_t pid;
+			pid = fork();
+			
+			//check whether fork success or not
+			if(pid == -1){
+				perror("Error: fork() failed\n");
+				return EXIT_FAILURE;
+			}
+			
+			if(pid == 0){
+				//in FIRST Child
+				if(multiTask == 1 && result2 == 1){
+					//if we have IPC Need
+					dup2(p[1], fileno(stdout));
+					close(p[0]);
+				}else{
+					//if we have no IPC needed or the second process is not valid
+					close(p[0]);
+					close(p[1]);
+				}
+				execv(path,argv);
+			}
+			else{
+				//in the parent
+				//first child wait pid part
+				close(p[0]);
+				close(p[1]);
+				if(background == 0){
+					//if process runs foreground
+					pid_t child_pid = waitpid(pid, &status, 0);
+					printf("[process %d terminated with exit status %d]\n", child_pid, status);
+					
+				}else{
+					//if process runs background
+					pid_t child_pid = waitpid(pid, &status, WNOHANG);
+					has_pid1 = 1;
+					bg_pid1 = pid;
+					if(child_pid > 0){
+						printf("[process %d terminated with exit status %d]\n", pid, status);
+						has_pid1 = 0;
+						bg_pid1 = -1;
+					}
+					
+				}
+				//if there is the second child, wait pid for that
+				if(multiTask == 1 && result2 == 1){
+					//if we have valid second process and IPC needed
+					pid_t pid2;
+					pid2 = fork();
+					
+					if(pid2 == -1){
+						perror("Error: fork() failed\n");
+						return EXIT_FAILURE;
+					}
+					
+					if(pid2 == 0){
+						//In Second Child
+						dup2(p[0], fileno(stdin));
+						close(p[1]);
+						execv(path2, argv2);
+					}
+					else{
+						if(background == 0){
+							pid_t child_pid = waitpid(pid2, &status, 0);
+							printf("[process %d terminated with exit status %d]\n", child_pid, status);
+						}else{
+							pid_t child_pid = waitpid(pid, &status, WNOHANG);
+							has_pid2 = 1;
+							bg_pid2 = pid2;
+							if(child_pid > 0){
+								printf("[process %d terminated with exit status %d]\n", pid2, status);
+								has_pid2 = 0;
+								bg_pid2 = -1;
+							}
+							
+						}
+					}
+				}
+				
+				
+			}
+		}else if(result == 0){
+			fprintf(stderr, "ERROR: command \"%s\" not found\n", argv[0]);
+		}
+		
+		//printf("[%s]\n", rBuffer);
+		
+		for(int i = 0; i < count; i++){
+			free(argv[i]);
+		}
+		if(multiTask == 1){
+			for(int i = 0; i < count2; i++){
+				free(argv2[i]);
+			}
+			
+		}
+		free(argv2);
+		free(argv);
+		free(path);
+		free(path2);
+		free(mypath);
+	
 	}
 	
 	
--- HW2/test.c
+++ HW2/test.c
@@ -7,10 +7,113 @@
 #include <sys/wait.h>
 #include <string.h>
 
+int checkPath(char* mypath, char* target, char* dic){
+	//check whether the path and target are valid
+	fprintf(stderr,"[%s],[%s],[%s]\n", mypath, target, dic);
+	if(mypath == NULL || target == NULL || dic == NULL){
+		fprintf(stderr,"Error: check Path read NULL char* \n");
+		fprintf(stderr,"[%s],[%s],[%s]\n", mypath, target, dic);
+		return -1;
+	}
+	//then check the path from left to right
+	else{
+		//fprintf(stderr,"[%s],[%s],[%s]\n", mypath, target, dic);
+		char* token = strtok(mypath, "#");
+		while(token != NULL){
+			char* temp = calloc(1024, sizeof(char));
+			strcpy(temp, token);
+			strcat(temp, "/");
+			strcat(temp, target);
+			printf("the temp is: %s \n", temp);
+			
+			//check the path
+			struct stat sb;
+			int result = lstat(temp, &sb);
+			//if find the path
+			if(result == 0 && (sb.st_mode & S_IXUSR)){
+				dic = strcpy(dic,temp);
+				free(temp);
+				printf("find at: %s\n",dic);
+				return 1;
+			}
+			token = strtok(NULL,"#");
+			free(temp);
+		}
+		return 0;
+	}
+}
 
 int main(){
-	
+	char* p = calloc(1024, sizeof(char));
+	getenv("MYPATH");
+	p = getenv("MYPATH");
+	printf("%s \n", p);
 	char* s = calloc(1024, sizeof(char));
+	s = strcpy(s, "ls -l");
+	
+	char* c = calloc(1024, sizeof(char));
+	c = strcpy(c, "meme.exe WOWS XXXX");
+	
+	char** instr = calloc(16, sizeof(char*));
+	int count = 0;
+	char* token = strtok(s," ");
+	printf("%s\n", token);
+	//add each instructions into the array
+	while(token != NULL){
+		char* temp = malloc(strlen(token) + 1);
+		strcpy(temp, token);
+		instr[count] = temp;
+		count++;
+		token = strtok(NULL, " ");
+	}
+	int result;
+	char* dir = calloc(1024, sizeof(char));
+	//check the MYPATH env variable
+	char* mypath = calloc(1024, sizeof(char));
+	//if $MYPATH is not set
+	
+	if( getenv("$MYPATH") == NULL ){
+		mypath = strcpy(mypath, "/bin#.");
+		result = checkPath(mypath, instr[0], dir);
+		
+	}else{
+		mypath = getenv("$MYPATH");
+		result = checkPath(mypath, instr[0], dir);
+	}
+	
+	
+	if(result == 1){
+		pid_t pid;
+		pid = fork();
+		
+		//printf("the dir is: %s\n", dir);
+		
+		if(pid == -1){
+			perror("fork failed\n");
+			return EXIT_FAILURE;
+		}
+			
+		if(pid == 0){
+			//char* argv[] = {"helloWorld.exe",NULL};
+			execv(dir,instr);	
+		}
+		else{
+			int status;
+			pid_t child_pid = waitpid(pid,&status,0);
+			printf("[process %d terminated with exit status %d]\n", child_pid, status);	
+		}
+	
+	}
+	
+	free(dir);
+	free(mypath);
+	for(int i = 0; i < count; i++){
+		free(instr[i]);
+	}
+	free(instr);
+	
+	/*
+	
 	if(getenv("$MYPATH") == NULL){
 		free(s);
 		printf("$MYPATH is not set\n");
@@ -20,6 +123,11 @@ int main(){
 		free(s);
 	}
 	
+	*/
+	
+	free(s);
+	free(c);
+	//printf("finished\n");
 	return EXIT_SUCCESS;
 	
 }
